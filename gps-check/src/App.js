efe5d3257e0e229dc716c323df7acc4b4def970d
islem kacem <islemkacem2@gmail.com>
2023-08-05 10:26:32 +0100

    gps-check

---
+++ gps-check/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+    <div class="mapouter">
+<div class="gmap_canvas">
+  <h3>GPS CHECKPOINT</h3>
+    <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=2%C3%A8me%20%C3%A9tage,%20GOMYCODE%20Tunis%20Centre%20Ville,%20Rue%20Charles%20de%20Gaulle,%20Tunis&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+      </iframe>
+      <a href="https://www.whatismyip-address.com">
+       </a>
+       <a href="https://www.embedgooglemap.net">GPS</a>
+       </div>
+       </div>
+       </div>
+  );
+}
+
+
+export default App;
